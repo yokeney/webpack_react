db7d83bf3dbb83e1fb32befb012350ded432af91
yokeney <965660471@qq.com>
2018-07-10 17:43:03 +0800

    webapck

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+
+class App extends Component {
+    render() {
+        return (
+          111
+        )
+    }
+};
+
+ReactDom.render(
+  <App />,
+    document.getElementById('root')
+)
